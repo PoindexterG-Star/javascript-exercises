981021e61de4aa464120c14d55673ff1fbd3fb0d
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-03-03 14:06:03 -0500

    solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, nDelete) {
+    let a = array.indexOf(nDelete);
+    let removed = array.splice(a, 1);
+    return array;
 };
 
 // Do not edit below this line
