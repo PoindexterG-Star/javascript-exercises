e0812880b41e181d9661aad67462526159ec4c53
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-03-03 18:20:10 -0500

    solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let c = (temp - 32) * 0.55;
+  return parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let d = temp * 1.8 +32;
+  return parseFloat(d.toFixed(1));
 };
 
 // Do not edit below this line
