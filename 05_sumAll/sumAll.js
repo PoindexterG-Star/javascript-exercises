b70e74f9985716b195cc7ceeb1994115dbd1b9f3
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-03-03 17:41:10 -0500

    solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let a = 0;
+    for(i = num1; i < (num2 + 1); i++) {
+        a +=i;
+    }
+    return a;
 };
 
 // Do not edit below this line
